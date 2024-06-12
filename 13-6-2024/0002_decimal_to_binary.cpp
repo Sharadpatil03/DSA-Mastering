45e6fb156937653b039904fbdf6dca26ae4b397d
Sharad Patil <112636539+Sharadpatil03@users.noreply.github.com>
2024-06-12 19:53:36 +0530

    Add files via upload

---
+++ 13-6-2024/0002_decimal_to_binary.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// decimal to binary :
+// 1) approach 1 : divide wala
+// 2) approach 2 : xor operator .
+
+int decimalToBinary(int n){
+
+
+return 0;
+}
+// 
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    int ans=0, i=0;
+
+    while (n!=0){
+        int bit= n&1;
+        ans = (bit *pow(10, i)) + ans;
+        
+        n = n >>1;
+        i++;
+
+    }
+
+    cout << "Answer :" <<ans<<endl;
+
+  
+    return 0;
+}
